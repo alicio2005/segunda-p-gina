d8a139a4237047606b647a72ca888e5382ceb971
alicio2005 <fm471195@gmail.com>
2023-10-28 16:11:29 -0600

    Agrege el archivo 1 de python

---
+++ main1.py
@@ -0,0 +1,62 @@
+#import this
+"""c1 = {'a', 'b', 'c'}
+print(c1)
+
+texto = 'Hola mi nombre es miguél.'
+print(texto.title())
+print(texto.lower())
+print(texto.upper())
+print(texto.capitalize())
+print(texto.count('a'))
+print(texto.isdigit())
+print(texto.replace('H', 'h'))"""
+
+"""matris = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+print(matris[0][3])"""
+
+"""c1 = {'a', 'b', 'c', 'a', 'b', 'c'}
+c2 = {'d', 'f', 'g', 'd', 'f', 'g', 'b'}"""
+
+"""v1 = c1.union(c2)
+print(v1)"""
+
+"""c1.difference(c2)
+print(c1)
+
+c1.difference_update(c2)
+print(c1)"""
+
+"""v1 = c1.intersection(c2)
+print(v1)
+
+c1.intersection_update(c2)
+print(c1)"""
+
+"""c1.discard('a')
+print(c1)
+
+v1 = c1.union(c2)
+print(v1)"""
+
+"""array = []
+
+for x in range(0, 10):
+    if x * 4:
+        array.append(x)
+print(array)
+
+lista = [x * 4 for x in range(0, 10)]
+print(lista)"""
+
+"""l1 = ['nombre', 'ciudad', 'edad']
+l2 = ['Miguél', 'México', 17]
+print(list(zip(l1, l2)))
+
+
+l = [l, range(0, 10)]"""
+
